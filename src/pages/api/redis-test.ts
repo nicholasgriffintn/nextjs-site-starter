69136d20e3ac6bf05f27ca183d917168374438fc
Nicholas Griffin <me@nicholasgriffin.co.uk>
2022-03-10 03:55:00 +0000

    chore: updating redis test

--- src/pages/api/redis-test.ts
+++ src/pages/api/redis-test.ts
@@ -3,8 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { redisCache } from '$src/server/redis';
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-	await redisCache.set('test', 'hello!');
-	const redisTest = await redisCache.get('test2');
+	await redisCache.set('test', 'Hello from Redis!');
+	const redisTest = await redisCache.get('test');
 
 	console.log(redisTest);
 
