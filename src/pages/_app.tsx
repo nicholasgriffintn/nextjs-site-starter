459522560609e1b2e6f24ddc0b5de2706dfb6cb2
Nicholas Griffin <me@nicholasgriffin.co.uk>
2022-03-10 00:00:22 +0000

    feat: updating pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,40 @@
 import { DefaultSeo } from 'next-seo';
-import '../css/tailwind.css';
+import { SessionProvider } from 'next-auth/react';
 import { Provider as UrqlProvider } from 'urql';
+
+import '../css/tailwind.css';
+
 import { useUrqlClient } from '$src/lib/urql-client';
+import Header from './components/header';
 
 const App = ({ Component, pageProps }: any) => {
 	const urqlClient = useUrqlClient();
 	return (
-		<UrqlProvider value={urqlClient}>
-			<DefaultSeo
-				titleTemplate="%s | App Name"
-				title="Page"
-				description="App Discription"
-				openGraph={{
-					type: 'website',
-					locale: 'en_GB',
-					url: 'https://app.url/',
-					title: 'App Name',
-					site_name: 'App Name',
-					description: 'App Description',
-					images: [{ url: '/images/social.png' }],
-				}}
-				twitter={{
-					handle: '@app_handle',
-					site: '@app_handle',
-					cardType: 'summary_large_image',
-				}}
-			/>
-			<Component {...pageProps} />
-		</UrqlProvider>
+		<SessionProvider session={pageProps.session}>
+			<UrqlProvider value={urqlClient}>
+				<DefaultSeo
+					titleTemplate="%s | App Name"
+					title="Page"
+					description="App Discription"
+					openGraph={{
+						type: 'website',
+						locale: 'en_GB',
+						url: 'https://app.url/',
+						title: 'App Name',
+						site_name: 'App Name',
+						description: 'App Description',
+						images: [{ url: '/images/social.png' }],
+					}}
+					twitter={{
+						handle: '@app_handle',
+						site: '@app_handle',
+						cardType: 'summary_large_image',
+					}}
+				/>
+				<Header />
+				<Component {...pageProps} />
+			</UrqlProvider>
+		</SessionProvider>
 	);
 };
 
